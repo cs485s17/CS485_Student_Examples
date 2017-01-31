6ef4e2b70f070771f94a9da30da152bb3f934800
chaddcw <chaddcw+github@gmail.com>
2017-01-31 15:26:30 -0800

    added proper comments

--- CS485_Student_Examples/01_CopyConstructorAndAssignmentOp/bigger.cpp
+++ CS485_Student_Examples/01_CopyConstructorAndAssignmentOp/bigger.cpp
@@ -9,36 +9,94 @@
 
 #include "bigger.h"
 
+//***************************************************************************
+// Constructor: bigger
+//
+// Description: sets mID and prints a message.
+//
+// Parameters:  None
+//
+// Returned:    None
+//***************************************************************************
 bigger::bigger ()
 {
   mID = getID ();
   std::cout << "ctor " << mID << "\n";
 }
 
+//***************************************************************************
+// Constructor: bigger
+//
+// Description: sets mID, sets mData to the parameter, and prints a message.
+//
+// Parameters:  x – the number to set to mDaa
+//
+// Returned:    None
+//***************************************************************************
 bigger::bigger (int x) :mData (x)
 {
   mID = getID ();
+  mData = x;
   std::cout << "ctor(int)" << mID << "\n";
 }
 
+//***************************************************************************
+// Constructor: bigger
+//
+// Description: copies the mData value from the parameter and prints a message
+//              a unique ID is generated.
+//
+// Parameters:  rcData – the object to be copied
+//
+// Returned:    None
+//***************************************************************************
+bigger::bigger (const bigger& rcData)
+{
+  mID = getID ();
+  mData = rcData.mData;
+  std::cout << "cctor" << mID << "\n";
+}
+
+//***************************************************************************
+// Destructor:  bigger
+//
+// Description: prints a message.
+//
+// Parameters:  None
+//
+// Returned:    None
+//***************************************************************************
 bigger::~bigger ()
 {
   std::cout << "dtor" << mID << "\n";
 }
 
-bigger::bigger (const bigger&rcData)
-{
-  mID = getID ();
-  std::cout << "cctor" << mID << "\n"; mData = rcData.mData;
-}
 
-bigger& bigger::operator=(const bigger&rcData)
+//***************************************************************************
+// Function:    operator=
+//
+// Description: Sets mData to the value in the parameter. Prints a message
+//
+// Parameters:  rcData   - the object to copy
+//
+// Returned:    the object operator= is called on.
+//***************************************************************************
+bigger& bigger::operator=(const bigger& rcData)
 {
   std::cout << "op=" << mID << "\n";
   mData = rcData.mData;
   return *this;
 }
 
+//***************************************************************************
+// Function:    getID
+//
+// Description: Generate a unique integer id.
+//
+// Parameters:  None
+//
+// Returned:    int - the id
+//***************************************************************************
 int bigger::getID () const
 {
   static int id = 0;

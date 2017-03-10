2a751f7f65a0fad2ee3bb48a23bb7c4230536725
chaddcw <chaddcw+github@gmail.com>
2017-03-10 11:46:54 -0800

    added more examples to exceptions

--- CS485_Student_Examples/06_ExceptionExample/driver.cpp
+++ CS485_Student_Examples/06_ExceptionExample/driver.cpp
@@ -11,6 +11,33 @@
 #include "CS485Exception.h"
 #include <exception>
 
+
+//***************************************************************************
+// Function:    riskyException
+//
+// Description: Demonstrate the risks of throwing an exception
+//
+// Parameters:  param - int to store in the new unsigned int
+//
+// Returned:    pointer to a new unsigned int
+//***************************************************************************
+//int cantThrowException (char data) noexcept;
+unsigned int *riskyException (int param) noexcept(false)
+{
+  CS485Exception cException;
+
+  unsigned int *pRetVal = new unsigned int;
+
+  if (0 > param)
+  {
+    throw std::range_error ("Negative value!");
+  }
+
+  *pRetVal = param;
+
+  return pRetVal;
+}
+
 //***************************************************************************
 // Function:    main
 //
@@ -23,10 +50,12 @@
 
 int main ()
 {
+  std::bad_alloc cNotAString;
+
+  unsigned int *pUInt = nullptr;
+
   int *paValues;
   int size = -1;
-  
-  std::bad_alloc cNotAString;
 
   try
   {
@@ -51,6 +80,24 @@ int main ()
     throw CS485Exception (42);
   }
   catch (const CS485Exception &e)
+  {
+    std::cout << e.what () << std::endl;
+  }
+
+  try
+  {
+    pUInt = riskyException (-9);
+  }
+  catch (const std::range_error &e)
+  {
+    std::cout << e.what () << std::endl;
+  }
+
+  try
+  {
+    pUInt = riskyException (-9);
+  }
+  catch (const std::exception &e)
   {
     std::cout << e.what () << std::endl;
   }
